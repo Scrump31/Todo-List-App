3a59318489491c2eafe5ec683a9e26e53fe26edc
Scottie Crump <scottiecrump@yahoo.com>
2016-07-14 15:23:52 -0400

    added function to repeat adding new tasks

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,3 +1,7 @@
 todoListApp.controller('MainCtrl', ['$scope', function($scope) {
-  
+  $scope.todoList = [];
+  $scope.newtask = function() {
+    $scope.todoList.push({done:false});
+    $scope.todoInput = "";
+  };
 }]);
