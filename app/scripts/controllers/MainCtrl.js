3c6c3f460ac2bf7ab77957334070337a903df245
Scottie Crump <scottiecrump@yahoo.com>
2016-07-15 15:27:16 -0400

    can now input new task and blur input field when click save

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,4 +1,5 @@
 todoListApp.controller('MainCtrl', ['$scope', function($scope) {
+  $scope.editing = true;
   $scope.todoList = [];
   $scope.newtask = function() {
     $scope.todoList.push({done:false});
