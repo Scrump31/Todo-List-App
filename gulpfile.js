7cb4da56c4818928f6a933b8170168ad94831d18
Scottie Crump <scottiecrump@yahoo.com>
2016-07-16 11:30:04 -0400

    added .container padding-bottom and gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ function strictmode() {
 }
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('app/scss/main.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('app/css'));
 });
 
